cab3656b6eb741b07611d7379e7d97d656cb2272
josh <josh@joshs-MacBook-Air.local>
2016-07-15 13:30:08 -0600

    got to a point where, i'm gonna move on. Still need to make it link to the search box for the filterBy param

--- app1-basics/controller.js
+++ app1-basics/controller.js
@@ -1,3 +1,11 @@
 angular.module("app").controller("ctrl",function($scope,service){
-	$scope.list = service.arrList;
+	$scope.data = service.data;
+	$scope.input = $scope.param;
+	$scope.filterBy = function(arr,term){
+		$scope.data = service.data;
+		console.log("in ctrl running filterBy");
+		$scope.list = service.filter(arr,term);
+		$scope.data = service.newData;
+	};
+
 });
