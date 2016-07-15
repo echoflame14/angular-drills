9f296d20ebceb8379580ee9d13e3a790c6e90d07
josh <josh@joshs-MacBook-Air.local>
2016-07-15 14:00:03 -0600

    basic skeleton for the pokeSearch app, ran into difficulty with the api being offline

--- app1-basics/controller.js
+++ app1-basics/controller.js
@@ -1,4 +1,8 @@
 angular.module("app").controller("ctrl",function($scope,service){
-
+	$scope.pokemonPic = "";
+	$scope.pokeSearch = function(x){
+		console.log("telling service to find " + x);
+		$scope.pokemonInfo = service.pokeSearch(x);
+	};
 
 });
--- app1-basics/service.js
+++ app1-basics/service.js
@@ -1,3 +1,11 @@
-angular.module("app").service("service", function(){
-
+angular.module("app").service("service", function($http){
+	this.pokeSearch = function(x){
+		console.log("service here contacting the api");
+		$http.get("http://pokeapi.co/api/v2/pokemon/" + x)
+			.then(function(response){
+				console.log("got a response");
+				console.log(response.data);
+				return response.data.name;
+			});
+	};
 });
