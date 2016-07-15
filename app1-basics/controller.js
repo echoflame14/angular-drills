c90dc4e684702911afdf566397a4dca7429bed37
josh <josh@joshs-MacBook-Air.local>
2016-07-15 13:31:38 -0600

    cleared the previous data for a clean slate

--- app1-basics/controller.js
+++ app1-basics/controller.js
@@ -1,11 +1,4 @@
 angular.module("app").controller("ctrl",function($scope,service){
-	$scope.data = service.data;
-	$scope.input = $scope.param;
-	$scope.filterBy = function(arr,term){
-		$scope.data = service.data;
-		console.log("in ctrl running filterBy");
-		$scope.list = service.filter(arr,term);
-		$scope.data = service.newData;
-	};
+
 
 });
--- app1-basics/service.js
+++ app1-basics/service.js
@@ -1,15 +1,3 @@
 angular.module("app").service("service", function(){
-	this.data = [1,2,3,1,234,1,2,3,3,3,3,3,1,1,2,2];
 
-	this.filter = function(arr, term){
-		console.log("in service.filter, sorting this " + arr + "by " + term);
-		this.newData = [];
-		for (var i = 0; i < arr.length; i++) {
-			if(arr[i] === term){
-				this.newData.push(arr[i]);
-			}
-		}
-		console.log(this.newData);
-		return this.newData;
-	};
 });
