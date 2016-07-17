d5f9e9585de852b68e92e6f554f4ef1061807926
josh <josh@joshs-MacBook-Air.local>
2016-07-17 15:37:46 -0600

    doing it the right way now for the file structure...

--- app1-basics/controller.js
+++ app1-basics/controller.js
@@ -1,8 +1,6 @@
 angular.module("app").controller("ctrl",function($scope,service){
-	$scope.pokemonPic = "";
-	$scope.pokeSearch = function(x){
-		console.log("telling service to find " + x);
-		$scope.pokemonInfo = service.pokeSearch(x);
+	$scope.alertTheText = function(someText){
+		alert(someText);
 	};
 
 });
